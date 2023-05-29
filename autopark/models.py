1636760b3579141545ccf46a1b303a514460c25d
Yagg-Yoon <61952790+Yagg-Yoon@users.noreply.github.com>
2023-05-30 00:47:14 +0200

    up to 5.1 before migration

---
+++ autopark/models.py
@@ -0,0 +1,39 @@
+from django.core.validators import RegexValidator
+from django.db import models
+
+# Create your models here.
+
+class Location(models.Model):
+    label = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.label
+    
+    class Meta:
+        ordering = ['label']
+
+class Vehicle(models.Model):
+    class VehicleType(models.TextChoices):
+        ELECTRIQUE = 'ELECTRIQUE'
+        ESSENCE = 'ESSENCE'
+        DIESEL = 'DIESEL'
+
+    description = models.TextField
+    number = models.CharField(
+        max_length=9,
+        validators=[
+            RegexValidator(
+                regex=r'^([A-Z]{2}-[0-9]{3}-[A-Z]{2})|([0-9]{1,4}[A-Z]{1,3}[0-9]{2})$',
+                message='Invalid format'
+            )
+        ]
+    )
+    vehicle_type = models.CharField(
+        max_length=15,
+        choices=VehicleType.choices,
+        default=VehicleType.ESSENCE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.number
--- tp1_yagg_yoon_laurent/settings.py
+++ tp1_yagg_yoon_laurent/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from dotenv import load_dotenv
 from pathlib import Path
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)l_k5a+5gqe28lm#*=no9tyq2ksfow&+lott0ukr+xhtm#p7))"
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# Environment
+ENVIRONMENT = os.environ.get('ENVIRONMENT', 'PRODUCTION')
+
+if ENVIRONMENT not in ['PRODUCTION', 'DEVELOPMENT']:
+    ENVIRONMENT = 'PRODUCTION'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ENVIRONMENT == 'PRODUCTION':
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -73,12 +86,27 @@ WSGI_APPLICATION = "tp1_yagg_yoon_laurent.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if ENVIRONMENT =='DEVELOPMENT':
+    # Local : SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    # Production : PostgreSQL
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
+        }
+    }
+
 
 
 # Password validation
@@ -109,9 +137,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = os.environ.get('STATIC_ROOT', '/path/to/static/files')
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', '/path/to/media/files')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Emails
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = bool(os.environ.get('EMAIL_USE_TLS', False))
